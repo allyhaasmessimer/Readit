d71466ae06b362fac16f38ff2219397607b805c7
ally haas <allyhaas@gmail.com>
2023-08-28 16:45:46 -0500

    allowed hosts updated

--- readit/settings.py
+++ readit/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['readit1-1f9246305140.herokuapp.com/']
+ALLOWED_HOSTS = ['readit1-1f9246305140.herokuapp.com', '.herokuapp.com']
+
 
 
 # Application definition
