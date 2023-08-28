e1c26bf02e6593a6c119b3b8fc55268675b6edea
ally haas <allyhaas@gmail.com>
2023-08-28 16:33:22 -0500

    deploy prep

--- readit/settings.py
+++ readit/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
-API_KEY = config('API_KEY')
+api_key = config("API_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
