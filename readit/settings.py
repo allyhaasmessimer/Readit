4aa08bcd9c3fc1a3d7e80bfe6a00ceb941007f24
ally haas <allyhaas@gmail.com>
2023-08-28 16:27:31 -0500

    deploy prep 3

--- readit/settings.py
+++ readit/settings.py
@@ -14,9 +14,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
+API_KEY = config('API_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
