ea234f558d628c20374ba32fcf20f8dad686217f
ally haas <allyhaas@gmail.com>
2023-08-28 15:30:11 -0500

    prep for deploy

--- readit/settings.py
+++ readit/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-md!hzqd_rj-2y=!#%3)*!^h*$$th*ueah%(9i-(p-tk)_d$qw$"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
