8791584cb8ca23c5c7d19160d0bc12625068ba33
ally haas <allyhaas@gmail.com>
2023-08-28 16:24:45 -0500

    more prep deploy

--- readit/settings.py
+++ readit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://readit1-1f9246305140.herokuapp.com/']
+ALLOWED_HOSTS = ['readit1-1f9246305140.herokuapp.com/']
 
 
 # Application definition
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
